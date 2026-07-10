1c91e0defbc1064c2f007729e6a420fbd991cb37
Jehoshua M <jehoshua.dev@gmail.com>
2026-07-10 21:31:33 +0530

    feat: persist and restore blockchain state using badger

---
+++ README.md
@@ -0,0 +1,6 @@
+# Go Blockchain
+
+A simple blockchain implementation written in Go for learning how blockchains work.
+It includes block creation, SHA-256 hashing, block chaining, and Proof of Work (PoW) mining.
+The project is built step by step to understand the core concepts behind blockchain technology.
+More features will be added and also bugs as development continues.
--- blockchain/block.go
+++ blockchain/block.go
@@ -1,8 +1,10 @@
 package blockchain
 
-type BlockChain struct {
-	Blocks []*Block
-}
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 type Block struct {
 	Hash     []byte
@@ -13,26 +15,45 @@ type Block struct {
 
 func CreateBlock(data string, prevHash []byte) *Block {
 	block := &Block{[]byte{}, []byte(data), prevHash, 0}
-	pow   := NewProof(block)
+	pow := NewProof(block)
 	nonce, hash := pow.Run()
 
-	block.Hash  = hash[:]
+	block.Hash = hash[:]
 	block.Nonce = nonce
 
 	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-
-	chain.Blocks = append(chain.Blocks, newBlock)
 }
 
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+func (block *Block) Serialize() []byte {
+	var res bytes.Buffer
+
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(block)
+
+	Handle(err)
+
+	return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewBuffer(data))
+
+	err := decoder.Decode(&block)
+
+	Handle(err)
+
+	return &block
+}
+
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
